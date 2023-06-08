80edb917c77983dfa35db3b9f3a7097693abe455
sueun <sueuniya1105@naver.com>
2023-06-08 23:00:28 +0900

    add new

---
+++ src/components/Card2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    <img :src="taking.image" :alt="`이미지${taking.id + 1}`" class="room-img"> 
+    <p>{{ taking.price }} 원</p> 
+    <p>{{ taking.content }}</p>
+    
+    <h4 @click="$emit('openModal', taking.id)">{{ taking.title }}</h4>
+    <!-- $emit('작명','데이터') 자식컴포먼트가 부모한테 보내는 것 -->
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'Card2' ,
+
+    props: {
+        taking : Object,
+    },
+    methods : {
+        send(){
+            this.$emit('openModal',this.원룸.id)
+            //@click="$emit('openModal', taking.id)" 이거 대신에 함수를 넣을 수 있음
+        }
+    }
+
+}
+</script>
---
+++ src/components/Discount.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="discount">
+        <h4>지금 결제하면 20% 할인</h4>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DisCount',
+}
+</script>
